24db72eee5aa06a84e610654da7199b106d8c0d8
DlLevelDesign <91367111+DlLevelDesign@users.noreply.github.com>
2021-10-01 15:38:52 -0400

    Update FlickeringLight.cs

--- FlickeringLight.cs
+++ FlickeringLight.cs
@@ -2,33 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*turns light on and off to simulate flickering effect*/
+/*A fast and dirty script made to get functionality quickly, with more time I would have made it more dynamic*/
 public class FlickeringLight : MonoBehaviour
 {
+    //is light on 
     bool isOn = false;
 
     public GameObject flashlight;
+    
+    //the countdown timer starts at 125  
+    int countdown = 125;
 
-    public int timer = 5;
-     int countdown;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        countdown = timer;
-        
-    }
-
-    // Update is called once per frame
+    //using fixedupdate for consistency 
     void FixedUpdate()
     {
+        //subtract 1 from countdown every frame 
         countdown = countdown - 1;
         
-
+        //if countdown equals 30 set isOn to false 
         if(countdown == 30)
         {
             isOn = false;
         }
-
+        
+        //if countdown equals 25 set isOn to true
         if (countdown == 25)
         {
             isOn = true;
@@ -49,11 +47,13 @@ public class FlickeringLight : MonoBehaviour
             isOn = true;
             countdown = timer;
         }
-
+        
+        //if isOn is true turn the light on
         if (isOn == true)
         {
             flashlight.SetActive(true);
         }
+        //if isOn is false turn the light off
         if(isOn == false)
         {
             flashlight.SetActive(false);
