a601795349a770da1a4d39a017ae9cc43f63a0f4
DlLevelDesign <91367111+DlLevelDesign@users.noreply.github.com>
2021-10-06 19:47:20 -0400

    Update ZombieTriggerSpawner.cs

--- ZombieTriggerSpawner.cs
+++ ZombieTriggerSpawner.cs
@@ -2,29 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*spawns an array of zombies when the player enters the trigger 
 public class ZombieTriggerSpawner : MonoBehaviour
 {
-
+    //the array of zombies to be spawned
     public GameObject[] zombies;
 
-
-    // Start is called before the first frame update
     void Start()
     {
+        //deactivate every zombie in the zombie array on the first frame
         foreach(GameObject zombie in zombies)
         {
             zombie.SetActive(false);
         }
-
+        
+        //turn off the mesh renderer of this object (is used to help with placement in the editor)
         this.GetComponent<MeshRenderer>().enabled = false;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
+    //if the player enters the trigger activate all of the zombies in the zombies array 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
@@ -34,7 +30,5 @@ public class ZombieTriggerSpawner : MonoBehaviour
                 zombie.SetActive(true);
             }
         }
-       
     }
-
 }
