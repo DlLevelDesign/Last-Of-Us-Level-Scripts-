4393be54ff4c7deeaf2649b9bc2337c35665260b
DlLevelDesign <91367111+DlLevelDesign@users.noreply.github.com>
2021-10-06 20:47:00 -0400

    Update PistolAmmoCounter.cs

--- PistolAmmoCounter.cs
+++ PistolAmmoCounter.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*track the amount of pistol ammo the player has*/
 public class PistolAmmoCounter : MonoBehaviour
-{
+{   
+    
     int maxLoadedAmmo = 3;
     public int loadedAmmo;
     int toBeLoaded;
@@ -11,17 +13,13 @@ public class PistolAmmoCounter : MonoBehaviour
 
     public GameObject pistol;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-      // pistol = GameObject.FindGameObjectWithTag("PlayerPistol");
-    }
-
     // Update is called once per frame
     void Update()
-    {
+    {   
+        //communicates with the Gun script that the LoadedBullets var from that script will equal the loadedAmmo var in this script 
         pistol.GetComponent<CoverShooter.Gun>().LoadedBullets = loadedAmmo;
-
+        
+        //if the pistol is active and the player fires it then subtract one bullet from the ammo count 
         if(pistol == true)
         {
             if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -29,22 +27,11 @@ public class PistolAmmoCounter : MonoBehaviour
                 loadedAmmo -= 1;
             }
         }
-
+        
+        //if the loaded ammo count is below zero set it to 0
         if(loadedAmmo < 0)
         {
             loadedAmmo = 0;
         }
-
-        //if (Input.GetKey(KeyCode.R))
-        //{
-        //    //pistol.GetComponent<CoverShooter.Gun>().LoadedBullets = ammoInventory - loadedAmmo;
-        //    if(ammoInventory < loadedAmmo && loadedAmmo >=maxLoadedAmmo)
-        //    {
-        //        toBeLoaded = ammoInventory - loadedAmmo;
-        //        pistol.GetComponent<CoverShooter.Gun>().LoadedBullets += toBeLoaded;
-        //    }
-
-
-        //}
     }
 }
