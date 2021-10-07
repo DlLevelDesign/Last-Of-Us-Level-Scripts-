8f3d330d4c032a2cd6f23eb08dfc9ccc37be657e
DlLevelDesign <91367111+DlLevelDesign@users.noreply.github.com>
2021-10-06 20:30:25 -0400

    Update KnifePickUpTrigger.cs

--- KnifePickUpTrigger.cs
+++ KnifePickUpTrigger.cs
@@ -2,28 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*trigger that allows the player to add a knife to their inventory*/
 public class KnifePickUpTrigger : MonoBehaviour
 {
-
     GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+        //assign the player variable to the gameobject with the tag Player 
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerStay(Collider other)
-    {
+    {   
+        //if the player enters the trigger turn on presscui
         if (other.gameObject.tag == "Player")
         {
             player.GetComponent<PressCUi>().isInTrigger = true;
+            //if the player presses c in the trigger, add one knife to the current number of knives, turn off the presscui, and deactivate the parent of this object 
             if (Input.GetKeyDown(KeyCode.C))
             {
                 player.GetComponent<KnifeCounter>().numOfKnives += 1;
