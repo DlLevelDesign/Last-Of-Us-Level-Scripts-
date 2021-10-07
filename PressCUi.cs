86403bf1c183ddd2662f8d9071a0afa8057deea5
DlLevelDesign <91367111+DlLevelDesign@users.noreply.github.com>
2021-10-06 20:08:20 -0400

    Update PressCUi.cs

--- PressCUi.cs
+++ PressCUi.cs
@@ -3,26 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*displays the text for picking up an item */
+/*works with KnifePickUpTrigger.cs, MedKit.cs, PistolAmmo.cs*/
+
 public class PressCUi : MonoBehaviour
 {
-
+    //bool for if the player is in a trigger or not 
+    //is public so other scripts can alter the value
     public bool isInTrigger;
+    //the text object that will display the text 
     public Text pressC;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
+        //set the text to be blank so nothing is appearing on the screen 
         pressC.text = "";
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //if true change the text
         if(isInTrigger == true)
         {
             pressC.text = "Press C to pick up item";
         }
-
+        
+        //if false change the text to be blank
         if(isInTrigger == false)
         {
             pressC.text = "";
