169afa052ea198ad737787d6762092f4a82b24f9
DlLevelDesign <91367111+DlLevelDesign@users.noreply.github.com>
2021-10-06 20:12:56 -0400

    Update KnifeCounter.cs

--- KnifeCounter.cs
+++ KnifeCounter.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*track the number of knifes the player has*/
 public class KnifeCounter : MonoBehaviour
 {
-
+    //the number of knives the player currently has
     public int numOfKnives;
+    //the text displaying the current amount of knives 
     public Text knives;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        //display the word KNIVES and the current number of knives
         knives.text = "KNIVES " + numOfKnives;
-
+        
+        //if the amount of knives ever reaches 0 or below0 set the number of knives to 0
         if(numOfKnives <= 0)
         {
             numOfKnives = 0;
